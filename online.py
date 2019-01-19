f98ed8559e987f6da68de6a4f5d9997686a547c5
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-19 18:51:11 +0530

    Update online.py

--- online.py
+++ online.py
@@ -799,7 +799,7 @@ async def avatar2(ctx, user: discord.Member=None):
     if user is None:
         embed = discord.Embed(title=f'Avatar', description="Here's your avatar that you've requested...\n Don't misuse this cmd...", color=0XFF69B4)
         embed.add_field(name='User: {}'.format(ctx.message.author.name), value='Avatar:', inline=True)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/516953091656908810/519072295080296469/Joker.png') 
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
         embed.set_image(url = ctx.message.author.avatar_url)
         embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f'{ctx.message.author.avatar_url}')
         embed.timestamp = datetime.datetime.utcnow()
@@ -807,7 +807,7 @@ async def avatar2(ctx, user: discord.Member=None):
     else:
         embed = discord.Embed(title=f'Avatar', description="Here's your avatar that you've requested...\n Don't misuse this cmd...", color=0XFF69B4)
         embed.add_field(name='User: {}'.format(user.name), value='Avatar:', inline=True)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/516953091656908810/519072295080296469/Joker.png') 
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
         embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_image(url = user.avatar_url)
