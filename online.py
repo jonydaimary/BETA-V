a972c0866c3602ecd6189eefa8bbba5ba5801903
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-19 20:49:53 +0530

    Update online.py

--- online.py
+++ online.py
@@ -516,7 +516,7 @@ async def friend(ctx, user:discord.Member,):
 @client.command(pass_context=True)
 async def ownerinfo(ctx):
     embed = discord.Embed(title="Information about owner", description="bot name- @DAB#1253 ", color=0XFF69B4)
-    embed.set_field(name=" bot owner name- @marcos.#0290 ")
+    embed.add_field(name=" bot owner name- @marcos.#0290 ")
     embed.add_field(name="Site- coming soon...", value="Thanks for adding our bot", inline=True)
     embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
     embed.timestamp = datetime.datetime.utcnow()
