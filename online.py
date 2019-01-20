95fb2992a8665644f3293960b003ff86e822a684
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-20 19:42:15 +0530

    Update online.py

--- online.py
+++ online.py
@@ -59,10 +59,10 @@ async def on_message(message):
 	
 @client.command(pass_context = True)
 async def meme(ctx):
+    embed = discord.Embed(title="{} meme",Color=0Xf9fcfc)
     async with aiohttp.ClientSession() as session:
         async with session.get("https://api.reddit.com/r/me_irl/random") as r:
-            data = await r.json()
-            embed = discord.Embed(title="{} meme",Color=0Xf9fcfc)
+            data = await r.json()          
             embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
             embed.set_footer(text=f'Requested by: {ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
             embed.timestamp = datetime.datetime.utcnow()
