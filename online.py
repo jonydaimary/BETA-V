6877979ac49dd1845429c01e9eba62e593337abf
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-20 14:53:23 +0530

    Update online.py

--- online.py
+++ online.py
@@ -527,8 +527,7 @@ async def avatar(ctx, user: discord.Member=None):
         await client.say('Please tag a person to change nickname. Example- ``mv!avatar <@user>``')
         embed = discord.Embed(color=0Xf9fcfc)
         embed.add_field(name='User: {}'.format(ctx.message.author.name), value='Avatar:', inline=True)
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
-        embed.set_image(url = ctx.message.author.avatar_url)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp')       
         embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f'{ctx.message.author.avatar_url}')
         embed.timestamp = datetime.datetime.utcnow()
         await client.say(embed=embed)
@@ -538,6 +537,7 @@ async def avatar(ctx, user: discord.Member=None):
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
         embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
         embed.timestamp = datetime.datetime.utcnow()
+        embed.set_image(url = user.avatar_url)
         await client.say(embed=embed)
 
 	
