961a94fdcc09eac835e145150ff13edd67a9bcf2
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-20 15:01:30 +0530

    Update online.py

--- online.py
+++ online.py
@@ -520,23 +520,25 @@ async def on_message_delete(message):
 		
 
 		
-
 @client.command(pass_context = True)
 async def avatar(ctx, user: discord.Member=None):
     if user is None:
-        await client.say('Please tag a person to change nickname. Example- ``mv!avatar <@user>``')
-        embed = discord.Embed(color=0Xf9fcfc)                 
+        embed = discord.Embed(title=f'Avatar', description="Here's your avatar that you've requested...\n Don't misuse this cmd...", color=0XFF69B4)
+        embed.add_field(name='User: {}'.format(ctx.message.author.name), value='Avatar:', inline=True)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
+        embed.set_image(url = ctx.message.author.avatar_url)
         embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f'{ctx.message.author.avatar_url}')
         embed.timestamp = datetime.datetime.utcnow()
         await client.say(embed=embed)
     else:
-        embed = discord.Embed(color=0Xf9fcfc)       
+        embed = discord.Embed(title=f'Avatar', description="Here's your avatar that you've requested...\n Don't misuse this cmd...", color=0XFF69B4)
+        embed.add_field(name='User: {}'.format(user.name), value='Avatar:', inline=True)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
         embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_image(url = user.avatar_url)
         await client.say(embed=embed)
 
-	
 	
 
 @client.command(pass_context=True)
