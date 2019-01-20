58ac5eabaff3e4a31d5b5169c952e196d3e3c536
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-20 15:17:34 +0530

    Update online.py

--- online.py
+++ online.py
@@ -726,7 +726,14 @@ async def thuglife(ctx):
 	
 
 	
-	
+@client.command(pass_context=True)
+async def help(ctx):
+    embed = discord.Embed(title="__Command Prefix:__ !! ", color=0Xf9fcfc)
+    embed.add_field(name="__**Commands**__", value="`avatar <user>` -  Avatar of mentioned user. \n`meme` - This will show a meme image. \n`slap <user>` - This will slap the user. \n`joke` - This will tell you a joke. ", inline=True)
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532088532576239647/536173122257420360/dab.webp') 
+    embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
+    embed.timestamp = datetime.datetime.utcnow()
+    await client.send_message(ctx.message.channel, embed=embed)	
 	
 	
 	
