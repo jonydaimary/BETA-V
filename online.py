51d6d369c5a8a96ed1022bcd6c7fe38073031ae2
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-21 12:39:11 +0530

    Update online.py

--- online.py
+++ online.py
@@ -456,8 +456,8 @@ async def serverinfo(ctx):
     join.add_field(name = '__Text/Voice Channels__', value = str(channelz));
     join.add_field(name = '__Roles (%s)__'%str(role_length), value = roles);
     join.add_field(name = '__Created__'%str(time), value = time);
-    embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
-    embed.timestamp = datetime.datetime.utcnow()
+    join.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
+    join.timestamp = datetime.datetime.utcnow()
     return await client.say(embed = join);
 
 
