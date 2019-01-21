300a2e2e7b0ce6b79806270ac8bf0dd9bb216af1
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-21 17:54:44 +0530

    Update online.py

--- online.py
+++ online.py
@@ -624,7 +624,8 @@ async def cat(ctx):
         data = json.loads(response.text)
         embed=discord.Embed(color=0Xf9fcfc)
         embed.set_author(name =  "Here's Your Cat {}".format(ctx.message.author.name),)
-        embed.set_image(url = data[0])	
+        embed.set_image(url = data[0])
+        embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
         await client.say(embed=embed)
     except:
         x = await client.say("Sorry, there was an error with the **cat** command")
