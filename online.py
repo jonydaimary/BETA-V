207bfb3598b8bbedddc61db94e0ae9b10b1e39ad
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-21 11:30:28 +0530

    Update online.py

--- online.py
+++ online.py
@@ -683,9 +683,7 @@ async def thuglife(ctx):
 	
 @client.command(pass_context=True)
 async def slap(ctx, user: discord.Member = None):
-    gifs = ["http://rs20.pbsrc.com/albums/b217/strangething/flurry-of-blows.gif?w=280&h=210&fit=crop", "https://media.giphy.com/media/LB1kIoSRFTC2Q/giphy.gif", "https://i.imgur.com/4MQkDKm.gif"]
-    if user.id == ctx.message.author.id:
-	await client.say("Goodluck slaping yourself {}".format(ctx.message.author.mention))
+    gifs = ["http://rs20.pbsrc.com/albums/b217/strangething/flurry-of-blows.gif?w=280&h=210&fit=crop", "https://media.giphy.com/media/LB1kIoSRFTC2Q/giphy.gif", "https://i.imgur.com/4MQkDKm.gif"]   
     if user == None:
         await client.say(f"{ctx.message.author.mention} ```Proper usage is\n\n>slap <mention a user>```")
     else:
