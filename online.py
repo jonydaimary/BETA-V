47f8c1f769371b67106ad1ea0b42e40b50d1d847
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 12:06:33 +0530

    Update online.py

--- online.py
+++ online.py
@@ -813,13 +813,15 @@ async def inviteb(ctx):
     await client.say(embed=embed)		
 
 	
+
 @client.command(pass_context = True)
 async def detailedinvites(ctx,*,user:discord.Member=None):
     invite = await client.invites_from(ctx.message.server)
     if user is None:
         for invite in invite:
           if invite.inviter == ctx.message.author:
-              embed = discord.Embed(color = 0xf9fcfc)
+              r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+              embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
               embed.add_field(name = 'Link used for inviting:',value =f'{invite.url}'.format(), inline=False)
               embed.add_field(name = 'Invites from this link:',value =f'{invite.uses}', inline=False)
               embed.add_field(name = 'Created at:',value =f'{invite.created_at}', inline=False)
@@ -829,15 +831,14 @@ async def detailedinvites(ctx,*,user:discord.Member=None):
     else:
         for invite in invite:
           if invite.inviter == user:
-              embed = discord.Embed(color = 0xf9fcfc)
+              r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+              embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
               embed.add_field(name = 'Link used for inviting:',value =f'{invite.url}'.format(), inline=False)
               embed.add_field(name = 'Invites from this link:',value =f'{invite.uses}', inline=False)
               embed.add_field(name = 'Created at:',value =f'{invite.created_at}', inline=False)
               embed.add_field(name = 'Channel:',value =f'{invite.channel}', inline=False)
               embed.add_field(name = 'ID:',value =f'{invite.id}', inline=False)
-              await client.say(embed=embed)
-					
-	
+              await client.say(embed=embed)	
 
 @client.command(pass_context=True)
 async def movie(ctx, *, name:str=None):
