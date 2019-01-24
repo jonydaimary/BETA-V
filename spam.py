be5ba005bb31c1f74ce017216193ceb43440a43f
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 20:55:39 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -33,13 +33,17 @@ async def on_ready():
  
 
 
+#######################
+## Made by Coco#6429 ##
+#######################
+@bot.command(pass_context=True)
 async def spam(ctx, count: int, *, SecretCocoSpam: str):
     if ctx.message.author.id == "498378677512437762":
         await bot.delete_message(ctx.message)
         for i in range(count):
-            await asyncio.sleep(5)
+            await asyncio.sleep(0.20)
             await bot.say(SecretCocoSpam)
-            
+ 
             
 @bot.command(pass_context=True)
 async def spam2(ctx, count: int, *, SecretCocoSpam: str):
