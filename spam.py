b008d4d9edf6d32da1648df59d613d291f70258b
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 20:05:06 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,13 +1,22 @@
 import discord
-from discord.ext.commands import bot
 from discord.ext import commands
-import random
+from discord.ext.commands.cooldowns import BucketType
 import asyncio
+import colorsys
+import random
+import platform
+from discord import Game, Embed, Color, Status, ChannelType
+import os
+import functools
 import time
+import datetime
+import requests
+import json
+import aiohttp		
 
-##PREFIX##
-bot = commands.Bot(command_prefix='!!')
 
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0xf9fcfc)
+client = commands.Bot(description="marcos bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
 ##BOT IS READY## 
 @bot.event
 async def on_ready():
