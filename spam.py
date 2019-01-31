e61e3a6d907c4e790d2ddcc370995a5404254f75
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 20:40:06 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -16,9 +16,8 @@ import json
 import aiohttp	
 
 ##PREFIX##
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 bot = commands.Bot(description="marcos Official Bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
-client = commands.Bot(description="marcos Official Bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
+client = commands.Bot(command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
 
 
 bot.remove_command('help')
