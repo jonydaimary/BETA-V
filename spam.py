19ae70aa74bafd73ac700a0591cd8a29456f37e4
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 20:00:22 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,68 +1,24 @@
-################################
-################################
-##### made by marcos.#0290  ####
-################################
-################################
 import discord
+from discord.ext.commands import bot
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
-import asyncio
-import colorsys
 import random
-import platform
-from discord import Game, Embed, Color, Status, ChannelType
-import os
-import functools
+import asyncio
 import time
-import datetime
-import requests
-import json
-import aiohttp		
 
+##PREFIX##
+bot = commands.Bot(command_prefix='!!')
 
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0xf9fcfc)
-client = commands.Bot(description="marcos bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
-client.remove_command('help')
-
-@client.event
+##BOT IS READY## 
+@bot.event
 async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-    print('--------')
-    print('--------')
-    print('Started New here ')
-    print('Created by marcos')
-    client.loop.create_task(status_task())
-
-
-###########################
-## Made by marcos.#0290  ##
-###########################
-@client.command(pass_context=True)
+    print("Bot Is Online! And Ready To Spam")
+ 
+@bot.command(pass_context=True)
 async def spam(ctx, count: int, *, SecretCocoSpam: str):
     if ctx.message.author.id == "498378677512437762":
         await bot.delete_message(ctx.message)
         for i in range(count):
-            await asyncio.sleep(1.0)
+            await asyncio.sleep(0.20)
             await bot.say(SecretCocoSpam)
- 
-            
-@client.command(pass_context=True)
-async def spam2(ctx, count: int, *, SecretCocoSpam: str):
-    if ctx.message.author.id == "498378677512437762":
-        await bot.delete_message(ctx.message)
-        for i in range(count):
-            await asyncio.sleep(0.5)
-            await bot.say(SecretCocoSpam)
-            
-####################################
 
-
-@client.command(pass_context = True)
-async def lovedetect(ctx, channel: discord.Channel=None, *, msg: str=None):
-    member = ctx.message.author
-    if channel is None or msg is None:
-        await client.say('```Proper usage is \n!!lovedetect @user1 @user2```')
-
-
-
-client.run(os.getenv('Token')) 
+bot.run(os.getenv('Token')) 
